2a72723902dd78120ddaa107490ba658d79f26a2
Veigabriel24 <gabrielveig@outlook.com>
2020-07-30 22:41:45 -0300

    Esse é top

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,104 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-  return(
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#000000',
+  };
+  const [categorias, setCategorias] = useState([]);
+
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (response) => {
+      const dados = await response.json();
+      setCategorias([
+        ...dados,
+      ]);
+    });
+  }, []);
+
+  function handleChange(event) {
+    setValue(
+      event.target.getAttribute('name'),
+      event.target.value,
+    );
+  }
+
+  function handleSubmit(infosDoEvento) {
+    infosDoEvento.preventDefault();
+    setCategorias([
+      ...categorias,
+      values,
+    ]);
+
+    setValues(valoresIniciais);
+  }
+
+  return (
     <PageDefault>
       <h1>
-        Cadastro de Categoria
+        Cadastro de Categoria:
+        {values.nome}
       </h1>
-      <Link to='/cadastro/categoria'>
-        Cadastrar Categoria
+
+      <form onSubmit={handleSubmit}>
+
+        <FormField
+          label="Nome da categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+
+        <ul>
+          {categorias.map((categoria, indice) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          ))}
+        </ul>
+
+      </form>
+
+      <Link to="/">
+        Ir para home
       </Link>
     </PageDefault>
   );
